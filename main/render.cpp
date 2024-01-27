daf4bdec1f80125a2215243ad128548e00f755b2
Olivier Wagener <wagener_olivier@hotmail.com>
2024-01-27 22:17:51 +0100

    fix: add % back to action buttons

--- main/render.cpp
+++ main/render.cpp
@@ -226,28 +226,28 @@ void renderActions(WiFiClient client, bool showActions) {
     class="btn btn-primary text-white me-2 mb-2"
     onclick="sendValue('turnAllChannelsOff','1')"
   >
-    0%
+    0 %%
   </button>
 
   <button
     class="btn btn-primary text-white me-2 mb-2"
     onclick="sendValue('turnAllChannels25','1')"
   >
-    25%
+    25 %%
   </button>
 
   <button
     class="btn btn-primary text-white me-2 mb-2"
     onclick="sendValue('turnAllChannels50','1')"
   >
-    50%
+    50 %%
   </button>
 
   <button
     class="btn btn-primary text-white me-2 mb-2"
     onclick="sendValue('turnAllChannels100','1')"
   >
-    100%
+    100 %%
   </button>
 
   <button
