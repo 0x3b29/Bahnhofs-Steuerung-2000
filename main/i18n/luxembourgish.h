973c1ac1646a6d6dc2a648f8e79349cd17b2ef63
Olivier Wagener <wagener_olivier@hotmail.com>
2024-02-10 11:07:22 +0100

    chore: fix some of the luxembourgish translation

--- main/i18n/luxembourgish.h
+++ main/i18n/luxembourgish.h
@@ -7,12 +7,12 @@
 #define I18N_OPTIONS_COMPACT_VIEW "Kompakt Vue"
 #define I18N_OPTIONS_FORCE_ALL_OFF "All Kanäl permanent op 0%"
 #define I18N_OPTIONS_FORCE_ALL_ON "All Kanäl permanent op 100%"
-#define I18N_OPTIONS_RANDOM_EVENTS "Zoufälleg Evenementer"
-#define I18N_OPTIONS_PROPAGATE_EVENTS "Verbindunge aktiv"
+#define I18N_OPTIONS_RANDOM_EVENTS "Zoufälleg Un/Aus"
+#define I18N_OPTIONS_PROPAGATE_EVENTS "Verbindungen aktiv"
 #define I18N_OPTIONS_CRAZY_BLINK "Verréckt Blénken"
 
 #define I18N_ACTIONS_EVEN "Gerued"
-#define I18N_ACTIONS_ODD "Ongeraued"
+#define I18N_ACTIONS_ODD "Ongerued"
 
 #define I18N_EDIT_CHANNEL "Kanal"
 #define I18N_EDIT_EDIT "Änneren"
@@ -20,7 +20,7 @@
 #define I18N_EDIT_START_STATE "Ufankszoustand"
 #define I18N_EDIT_BRIGHTNESS "Hellegkeet"
 #define I18N_EDIT_RANDOM_ON "Zoufälleg Un"
-#define I18N_EDIT_RANDOM_FREQ "~ Evenementer/Stonn"
+#define I18N_EDIT_RANDOM_FREQ "~ Un/Aus pro Stonn"
 #define I18N_EDIT_RANDOM_OFF "Zoufälleg Aus"
 #define I18N_EDIT_LINKED "Verbonnen"
 #define I18N_EDIT_CONTROLLED_BY_CHANNEL "Gesteiert duerch Kanal"
@@ -28,13 +28,13 @@
 #define I18N_EDIT_SAVE "Späicheren"
 
 #define I18N_CHANNEL_CHANNEL "Kanal"
-#define I18N_CHANNEL_BOARD "Bried"
+#define I18N_CHANNEL_BOARD "Platinn"
 #define I18N_CHANNEL_PIN "Pin"
 #define I18N_CHANNEL_DESCRIPTION "Beschreiwung"
 #define I18N_CHANNEL_START_STATE "Ufankszoustand"
 #define I18N_CHANNEL_BRIGHTNESS "Hellegkeet"
 #define I18N_CHANNEL_RANDOMLY_ON "Zoufälleg Un"
-#define I18N_CHANNEL_RANDOM_FREQ "~ Evenementer/Stonn"
+#define I18N_CHANNEL_RANDOM_FREQ "~ Un/Aus pro Stonn"
 #define I18N_CHANNEL_RANDOMLY_OFF "Zoufälleg Aus"
 #define I18N_CHANNEL_LINKED "Verbonnen"
 #define I18N_CHANNEL_COMMANDED_BY_CHANNEL "Gesteiert duerch Kanal"
@@ -48,9 +48,9 @@
 #define I18N_HEADING_CHANNELS "Kanäl"
 
 #define I18N_WARNING_RECURSION_BEFORE_MAX                                      \
-  "Warnung: Eng Schleef oder ze déif Verschachtelung (méi wéi"
+  "Warnung: Eng Schleef oder ze déif Verschachtlung (méi wéi"
 #define I18N_WARNING_RECURSION_AFTER_MAX                                       \
   "Niveauen) gouf an de verbonne Kanäl entdeckt. Iwwerpréift w.e.g all Verbindungen "    \
-  "op Schleefen oder erhéicht déi maximal Verschachtelungstiefe!"
+  "op Schleefen oder erhéicht déi maximal Verschachtlungsdéift!"
 
 #endif // luxembourgish_h
