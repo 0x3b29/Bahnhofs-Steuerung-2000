ad31d870fe74c7f8c5566536299149600423aad1
Olivier Wagener <wagener_olivier@hotmail.com>
2024-07-05 15:54:15 +0200

    chore: increase limit for PWM boards to 16. A few more are still possible but untested

--- main/main.h
+++ main/main.h
@@ -6,11 +6,11 @@
 #define st Serial.print
 #define sn Serial.println
 
-#define PWM_BOARDS 8
+#define PWM_BOARDS 16
 #define CHANNELS_PER_PWM_BOARD 16
 #define MAX_TOTAL_CHANNELS PWM_BOARDS * CHANNELS_PER_PWM_BOARD
 
-#define PWM_REFRESH_RATE  1042
+#define PWM_REFRESH_RATE 1042
 
 #define MAX_RECURSION 5
 
