592aeb0891eee5f2cee5141c4d80796f7357ab8e
Olivier Wagener <wagener_olivier@hotmail.com>
2024-11-12 23:13:10 +0100

    chore: add note about quotes to arduino secrets example file

--- main/example_arduino_secrets.h
+++ main/example_arduino_secrets.h
@@ -1,3 +1,4 @@
-// Copy this file, rename to arduino_secrets.h and fill in your wifi credentials.
+// Copy this file, rename to arduino_secrets.h and fill in your wifi
+// credentials. Please also note that the "" are nessesary.
 #define SECRET_SSID "Your_WiFi_Name"
 #define SECRET_PASS "Your_WiFi_Password"
